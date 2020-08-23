d70e034c7409366770e68ae8f13161d7f8a97b6f
yokn <yokn@protonmail.com>
2020-08-23 18:24:17 +0300

    add Game#change_current_player

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 
+require_relative 'board'
+require_relative 'player'
+
 class Game
   def initialize
     @board = Board.new
     @player1 = Player.new('⚪')
     @player2 = Player.new('⚫')
+    @current_player = @player1
+  end
+
+  def change_current_player
+    @current_player = @current_player == @player1 ? @player2 : @player1
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,8 +3,17 @@
 require_relative '../lib/game'
 
 describe Game do
-  context '' do
-    it '' do
+  context 'while the game is not over' do
+    subject(:game) { described_class.new }
+    let(:expected_player1) { game.instance_variable_get(:@player1) }
+    let(:expected_player2) { game.instance_variable_get(:@player2) }
+    let(:current_player) { game.instance_variable_get(:@current_player) }
+    it 'sets the current player' do
+      expect(current_player).to equal(expected_player1)
+    end
+    it 'changes the current player' do
+      game.change_current_player
+      expect(current_player).to equal(expected_player2)
     end
   end
 end
